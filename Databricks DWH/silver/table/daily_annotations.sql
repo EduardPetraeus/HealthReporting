68f6d9fc7bfb0d91219716fa23395bc61c03e095
EduardPetraeus <clauspetraeus@hotmail.com>
2025-12-19 15:34:11 +0000

    New table for adding comments to data

---
+++ Databricks DWH/silver/table/daily_annotations.sql
@@ -0,0 +1,8 @@
+CREATE TABLE workspace.silver.daily_annotations (
+  sk_date          INT NOT NULL,
+  annotation_type  STRING NOT NULL,  -- e.g. 'training', 'incident', 'work', 'travel'
+  annotation       STRING NOT NULL,
+  created_by       STRING,
+  is_valid         BOOLEAN NOT NULL
+)
+USING DELTA;
